1ae3c207c20d00a7e44af4fe8c9fe470c5094473
jk <jigme1968@gmail.com>
2015-03-13 01:22:01 +0800

    init

---
+++ src/Hschart-Tests.js
@@ -0,0 +1,9 @@
+define("amber-hschart/Hschart-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+$core.addPackage('Hschart-Tests');
+$core.packages["Hschart-Tests"].innerEval = function (expr) { return eval(expr); };
+$core.packages["Hschart-Tests"].transport = {"type":"amd","amdNamespace":"amber-hschart"};
+
+$core.addClass('HschartTest', $globals.TestCase, [], 'Hschart-Tests');
+
+});
